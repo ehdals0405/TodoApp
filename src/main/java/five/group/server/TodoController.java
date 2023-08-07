31191fa87e8b4a1d581c097944266c6b34c6cb6c
origin <git@github.com:ehdals0405/PreProjectback.git>
2023-08-07 15:54:16 +0900

    TodoApp

---
+++ src/main/java/five/group/server/TodoController.java
@@ -0,0 +1,13 @@
+package five.group.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
+
